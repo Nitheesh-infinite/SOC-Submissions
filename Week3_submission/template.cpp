853b511ba4dcfe3f5cb6e0f07fac0a797b2b4eb6
Nitheesh07 <niteeshvennela628@gmail.com>
2025-07-02 21:30:33 +0530

    Week3 added

---
+++ Week3_submission/template.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    // You can add more functionality here as needed
+    return 0;
+}
+// To compile this code, use the command:
+// g++ -std=c++17 -Wall -O2 example.cpp
+// Then run it with `./a.out` to see the output for the default type.
+
+
